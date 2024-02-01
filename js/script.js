2abd2a3ce694ce50868843c29e00a6cc73138f43
Aldidwsti <2021desember20212021@gmail.com>
2024-02-01 14:00:55 +0800

    <pesan deskriptif>

--- js/script.js
+++ js/script.js
@@ -9,3 +9,8 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "none";
 }
 }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
